f0a57d16caacf76cd138bb162362c6c60756c91b
Cedric Baetens <cedric.baetens@cbacomputing.be>
2021-03-18 19:58:53 +0100

    Removed .vs folder and moved ExampleClass

--- ExampleClass.cs
+++ ExampleClass.cs
@@ -1,8 +1,41 @@
 ﻿using System;
 
-public class Class1
+namespace Themes.BaelDark
 {
-	public Class1()
+	public interface IInterface
 	{
+
+	}
+	public class Class : IInterface
+	{
+		public int Property { get; set; }
+
+		private readonly string readonlyString = "Readonly";
+		private const string constantString = "Readonly";
+		private int _field = 0;
+
+		public void Method(int parameter)
+		{
+			var local = 1;
+
+			_field = parameter;
+			_field = local;
+		}
+	}
+
+	public static class Extensions
+	{
+		public static int ExtensionsMethod(this int value)
+		{
+			return value + 1;
+		}
+	}
+
+	public enum Enum
+	{
+		Value1,
+		Value2
 	}
 }
+
+
